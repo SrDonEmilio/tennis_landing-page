76809e5f69522739694598e031197fb8854aaa8d
SrDonEmilio <emilio@nextup.dev>
2020-07-30 02:05:06 -0500

    Updates

---
+++ precache-manifest.ea4a6a96959b2456250f21c56e5cba8a.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2241cc07ca11230bad2efe62f20b82e",
+    "url": "/tennis_landing-page/index.html"
+  },
+  {
+    "revision": "0ad2173c24a2d925609b",
+    "url": "/tennis_landing-page/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "41bcb688044efc27aa72",
+    "url": "/tennis_landing-page/static/css/main.3bc4f335.chunk.css"
+  },
+  {
+    "revision": "0ad2173c24a2d925609b",
+    "url": "/tennis_landing-page/static/js/2.8bf6b216.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tennis_landing-page/static/js/2.8bf6b216.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41bcb688044efc27aa72",
+    "url": "/tennis_landing-page/static/js/main.30e50920.chunk.js"
+  },
+  {
+    "revision": "b9bf4a5655962abef0e9",
+    "url": "/tennis_landing-page/static/js/runtime-main.1b4266de.js"
+  },
+  {
+    "revision": "542e30102e1f85bc27f0f5691617f2f4",
+    "url": "/tennis_landing-page/static/media/HelveticaNeue-Bold.542e3010.eot"
+  },
+  {
+    "revision": "7cc9b94a8e1b3c442e25be421f097eb9",
+    "url": "/tennis_landing-page/static/media/HelveticaNeue-Bold.7cc9b94a.woff"
+  },
+  {
+    "revision": "89078fdadd6274769293098171b817fd",
+    "url": "/tennis_landing-page/static/media/HelveticaNeue-Bold.89078fda.ttf"
+  },
+  {
+    "revision": "9c9117e6f942e3f27436ee0415cb5358",
+    "url": "/tennis_landing-page/static/media/HelveticaNeue-Bold.9c9117e6.svg"
+  },
+  {
+    "revision": "e6027db3a484d52902e31866339fb862",
+    "url": "/tennis_landing-page/static/media/HelveticaNeue-Bold.e6027db3.otf"
+  },
+  {
+    "revision": "07ec5a73bced8586bba26765b3bd3838",
+    "url": "/tennis_landing-page/static/media/HelveticaNeue.07ec5a73.svg"
+  },
+  {
+    "revision": "4bafbebdfdcdf132a294653bac2ad4a5",
+    "url": "/tennis_landing-page/static/media/HelveticaNeue.4bafbebd.woff"
+  },
+  {
+    "revision": "95d233318b737f979a86e7d17a15318b",
+    "url": "/tennis_landing-page/static/media/HelveticaNeue.95d23331.eot"
+  },
+  {
+    "revision": "a035ad376fc36ca17d91308451b8d015",
+    "url": "/tennis_landing-page/static/media/HelveticaNeue.a035ad37.ttf"
+  },
+  {
+    "revision": "ccad04d46768981ff847f22e8a53b5b3",
+    "url": "/tennis_landing-page/static/media/HelveticaNeueRegular.ccad04d4.ttf"
+  },
+  {
+    "revision": "f3e951711cd49ba603037f852f9076dd",
+    "url": "/tennis_landing-page/static/media/HelveticaNeueRegular.f3e95171.svg"
+  },
+  {
+    "revision": "f4920d35a9d47ffb7c624f382131c326",
+    "url": "/tennis_landing-page/static/media/HelveticaNeueRegular.f4920d35.eot"
+  },
+  {
+    "revision": "fcccfdfa6bf0c92c9a5ea2a705cb5641",
+    "url": "/tennis_landing-page/static/media/HelveticaNeueRegular.fcccfdfa.woff"
+  },
+  {
+    "revision": "ce546fef19234a3a3990cda0f7cdd235",
+    "url": "/tennis_landing-page/static/media/action-athlete-ball-342361.ce546fef.png"
+  },
+  {
+    "revision": "01082f95b0bf3f6d27b150404960cd3b",
+    "url": "/tennis_landing-page/static/media/ball-color-court-1405355.01082f95.jpg"
+  },
+  {
+    "revision": "ebe205af7a992967f923a8596e8ea085",
+    "url": "/tennis_landing-page/static/media/group-10.ebe205af.jpg"
+  },
+  {
+    "revision": "597722159950c0329383cb5826e5f26f",
+    "url": "/tennis_landing-page/static/media/group.59772215.jpg"
+  }
+]);
